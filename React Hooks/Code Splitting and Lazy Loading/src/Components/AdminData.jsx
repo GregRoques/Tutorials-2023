cd11774ec795a45bb63fcdd16767733abe213998
Greg Roques <42392687+GregRoques@users.noreply.github.com>
2023-11-11 14:49:01 -0500

    Delete React Hooks directory

--- React Hooks/Code Splitting and Lazy Loading/src/Components/AdminData.jsx
+++
@@ -1,7 +0,0 @@
-export default function AdminData() {
-  return (
-    <div>
-      <h2>You are an Admin</h2>
-    </div>
-  );
-}
--- React Hooks/Code Splitting and Lazy Loading/src/functions/slowLoading.js
+++
@@ -1,5 +0,0 @@
-export default function wait(time) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, time);
-  });
-} // simulate download loading time on large page
